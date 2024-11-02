f4da5745f5229da97cbd1163882ab123769d7930
Vinodh Kumar <vinodhkumar.manickam@freshworks.com>
2024-11-02 15:17:36 +0530

    chore: lint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,50 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { ecmaVersion: 2020, globals: globals.browser },
+  },
   {
     // Note: there should be no other properties in this object
     ignores: [
       'coverage',
-      'public',
-      'dist',
+      '**/public',
+      '**/dist',
       'pnpm-lock.yaml',
       'pnpm-workspace.yaml',
     ],
+  },
+  {
+    files: ['apps/web-app/**/*.{ts,tsx}'],
+    settings: { react: { version: '18.3' } },
+    languageOptions: {
+      // other options...
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: './apps/web-app',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
